5a4303e5a3279a34346bdfc07a4a46e4094211ed
gabriel <aballaygabriel@gmail.com>
2019-11-07 15:53:31 -1200

    login y usuario

---
+++ app/src/main/java/com/AballayGabriel/petfinder/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.AballayGabriel.petfinder.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    //private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        //mText = new MutableLiveData<>();
+        //mText.setValue("Perfil de Usuario");
+    }
+
+    public LiveData<String> getText() {
+        return null;// mText;
+    }
+}
